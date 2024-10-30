f86402430529603331ebb82f220f87b9c942fa0c
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-10-30 16:06:18 +0100

    Update 403View.vue

---
+++ src/views/error/403View.vue
@@ -0,0 +1,14 @@
+<!-- src/views/NotFound.vue -->
+<template>
+  <div>
+    <h1>403 - Access Denied</h1>
+    <p>Sorry, the page you are not authorized to see this page.</p>
+    <router-link to="/">Return to Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
