b3ffb89096850bb2680174d10ee38613860a2ec5
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-11-06 18:04:08 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 //general front constant to fill with backend endpoints
 
 const config = {
-  APIbaseUrl:'http://127.0.0.1/',
+  APIbaseUrl:'http://localhost/Sharalla/backend/',
   endpoints:{
     createUser:'',
     getUser:'',
