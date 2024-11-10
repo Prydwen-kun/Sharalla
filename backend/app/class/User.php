6bd618560bc708e634b1302f500035f5297fd39b
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-11-10 18:08:23 +0100

    Update User.php

--- backend/app/class/User.php
+++ backend/app/class/User.php
@@ -1,6 +1,15 @@
 <?php
 class User
 {
+    private $id;
+    private $username;
+    private $email;
+    private $last_login;
+    private $rank_id;
+    private $rank;
+    private $power;
+    private $avatar;
+
     public function __construct(array $data)
     {
         $this->hydrate($data);
@@ -14,5 +23,165 @@ class User
                 $this->$methodName($value);
             }
         }
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */ 
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of last_login
+     */ 
+    public function getLast_login()
+    {
+        return $this->last_login;
+    }
+
+    /**
+     * Set the value of last_login
+     *
+     * @return  self
+     */ 
+    public function setLast_login($last_login)
+    {
+        $this->last_login = $last_login;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of rank_id
+     */ 
+    public function getRank_id()
+    {
+        return $this->rank_id;
+    }
+
+    /**
+     * Set the value of rank_id
+     *
+     * @return  self
+     */ 
+    public function setRank_id($rank_id)
+    {
+        $this->rank_id = $rank_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of rank
+     */ 
+    public function getRank()
+    {
+        return $this->rank;
+    }
+
+    /**
+     * Set the value of rank
+     *
+     * @return  self
+     */ 
+    public function setRank($rank)
+    {
+        $this->rank = $rank;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of power
+     */ 
+    public function getPower()
+    {
+        return $this->power;
+    }
+
+    /**
+     * Set the value of power
+     *
+     * @return  self
+     */ 
+    public function setPower($power)
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of avatar
+     */ 
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set the value of avatar
+     *
+     * @return  self
+     */ 
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
     }
 }
