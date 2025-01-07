e15d3b32189ba6895beebd93f870d41de3eacbe9
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2025-01-07 11:05:36 +0100

    Update fileView.vue

--- src/views/files/fileView.vue
+++ src/views/files/fileView.vue
@@ -52,6 +52,16 @@ async function download(file_id, file_name, file_type, file_ext) {
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
+    } else {
+      const response = await axios.post(`${config.APIbaseUrl}${config.endpoints.files.download}${config.endpoints.GET.fileId}${file_id}`, { responseType: 'arraybuffer' })
+
+      const url = window.URL.createObjectURL(new Blob([response.data])); const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', file_name + '.' + file_ext);
+      // Set the default filename
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     }
 
   } catch (error) { console.error('Error downloading file:', error); }
